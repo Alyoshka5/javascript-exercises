a44d0f0d408d8ccacf3cdbe7a313dbe4cb951c2f
Alyoshka <alyoshkamyshko@gmail.com>
2022-03-19 23:19:24 -0700

    Complete first 7 javascript excercises (part of Fundamentals Part 4)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string = string.split('');
+    let result = [];
+    for (const char of string) {
+        result.unshift(char);
+    }
+    return result.join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rem) {
+    return arr.filter(value => !(rem.includes(value)));
 };
 
 // Do not edit below this line
